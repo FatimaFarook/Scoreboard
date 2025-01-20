dec8c8c2cc063a470e2bef443b42ce9d3f03d7de
fathima <mffathima16@gmail.com>
2025-01-20 12:18:32 +0530

    added functionality

---
+++ index.js
@@ -0,0 +1,25 @@
+let count = 0;
+function addOneHome() {
+    count += 1;
+    document.querySelector("#home-count").innerText = count;
+}
+function addTwoHome() {
+    count += 2;
+    document.querySelector("#home-count").innerText = count;
+}
+function addThreeHome() {
+    count += 3;
+    document.querySelector("#home-count").innerText = count;
+}
+function addOneGuest() {
+    count += 1;
+    document.querySelector("#guest-count").innerText = count;
+}
+function addTwoGuest() {
+    count += 2;
+    document.querySelector("#guest-count").innerText = count;
+}
+function addThreeGuest() {
+    count += 3;
+    document.querySelector("#guest-count").innerText = count;
+}
